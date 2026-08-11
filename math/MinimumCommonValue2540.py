637f2b7c5670a8f48b7c38d8d242d89dcbde65e6
ELias <elias2005new@gmail.com>
2026-08-11 16:43:17 +0300

    math q

---
+++ math/MinimumCommonValue2540.py
@@ -0,0 +1,16 @@
+# Runtime 0ms beats 100%
+# memory beats 51%
+class Solution:
+    def missingInteger(self, nums: List[int]) -> int:
+        target = 0
+        target += nums[0]
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1] + 1:
+                target += nums[i]
+            else:
+                break
+        while True:
+            if target in nums:
+                target += 1
+            else:
+                return target
