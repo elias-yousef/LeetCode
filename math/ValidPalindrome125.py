50a954cedf5fedde7279b96fcff94ab75c5ee80a
ELias <elias2005new@gmail.com>
2026-08-12 20:27:47 +0300

    bad question

---
+++ math/ValidPalindrome125.py
@@ -0,0 +1,17 @@
+# Runtime beats 17%
+# memory beats 76%
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        text = ""
+        for ch in s:
+            if ch.isalpha() or ch.isdigit():
+                text += ch
+        length = len(text) - 1
+        i = 0
+        while length > i:
+            if text[length] != text[i]:
+                return False
+            length -= 1
+            i += 1
+        return True
