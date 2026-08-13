4100b6971ff75e71a44d2d6a675e96df5512052b
ELias <elias2005new@gmail.com>
2026-08-13 04:22:00 +0300

    medium q

---
+++ strings/MultiplyStrings43.py
@@ -0,0 +1,7 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        num1 = int(num1)
+        num2 = int(num2)
+        awnser = num2 * num1
+        return str(awnser)
+        
